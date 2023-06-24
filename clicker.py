a4be630d70525b20e0abc4da38aaea60ac9df2d2
Jmomalley <122636503+Jmomalley@users.noreply.github.com>
2023-06-24 16:54:37 -0600

    updated some order and increased the timed wait

--- clicker.py
+++ clicker.py
@@ -14,13 +14,13 @@ downArrowY = 1030
 refreshButtonX = 90
 refreshButtonY = 57
 
-numStartingDownArrows = 20 #the number of down arrows you need to click to get to the first coupon
+numStartingDownArrows = 17 #the number of down arrows you need to click to get to the first coupon
 numBetweenDownArrows = 20 #the number of down arrows you need to click to get to the next coupon
 numClipsMax = 5
 
 numCouponsGetting = 1 #the number of coupons you want to get in a single run
 #numRuns = 5 #the number of times you want to run the program
-numRuns = 1 #the number of times you want to run the program
+numRuns = 2 #the number of times you want to run the program
 
 rangeMax = 69 #the number of clicks that are in the range
 rangeVal = 10 #the distance between each click
@@ -31,15 +31,12 @@ for item in range(numStartingDownArrows): #this it down to get to the first coup
     pyautogui.click(downArrowX, downArrowY)
 
 for k in range(numRuns): #this is the number of times you want to run the program
-    pyautogui.click(refreshButtonX, refreshButtonY) #refreshes the page
-
-
 
     for j in range(rangeMax):
             pyautogui.click(clickButtonX, clickButtonY + rangeVal * j)
 
-    for i in range(numBetweenDownArrows): #clicks the down arrow 6 times to get to the next coupon
-            pyautogui.click(downArrowX, downArrowY)
+    # for i in range(numBetweenDownArrows): #clicks the down arrow 6 times to get to the next coupon
+    #         pyautogui.click(downArrowX, downArrowY)
 
         # if numClips == numClipsMax: #helps to stop the precession of the coupons up the page
         #     pyautogui.click(downArrowX,downArrowY)
@@ -47,4 +44,5 @@ for k in range(numRuns): #this is the number of times you want to run the progra
         #     numClips = 0
         # else:
         #     numClips += 1
-    
+    pyautogui.click(refreshButtonX, refreshButtonY) #refreshes the page
+    time.sleep(3)

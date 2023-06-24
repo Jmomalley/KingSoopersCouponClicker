390e8f5db010c673722d868a3c2a3f950ffbfc1b
Jmomalley <122636503+Jmomalley@users.noreply.github.com>
2023-06-23 22:22:09 -0600

    worked on restructure

--- clicker.py
+++ clicker.py
@@ -8,31 +8,43 @@ import time
 # pyautogui.click(850, 980)
 
 clickButtonX = 850
-clickButtonY = 950
+clickButtonY = 350
 downArrowX = 955
 downArrowY = 1030
+refreshButtonX = 90
+refreshButtonY = 57
+
+numStartingDownArrows = 20 #the number of down arrows you need to click to get to the first coupon
+numBetweenDownArrows = 20 #the number of down arrows you need to click to get to the next coupon
 numClipsMax = 5
 
-numCouponsGetting = 150
+numCouponsGetting = 1 #the number of coupons you want to get in a single run
+#numRuns = 5 #the number of times you want to run the program
+numRuns = 1 #the number of times you want to run the program
 
-rangeMax = 9 #the number of clicks that are in the range
+rangeMax = 69 #the number of clicks that are in the range
 rangeVal = 10 #the distance between each click
 
 numClips = 0
 
-for item in range(3): #this it down to get to the first coupon
+for item in range(numStartingDownArrows): #this it down to get to the first coupon
     pyautogui.click(downArrowX, downArrowY)
 
-for i in range(20):
+for k in range(numRuns): #this is the number of times you want to run the program
+    pyautogui.click(refreshButtonX, refreshButtonY) #refreshes the page
+
+
 
     for j in range(rangeMax):
-        pyautogui.click(clickButtonX, clickButtonY + rangeVal * j)
+            pyautogui.click(clickButtonX, clickButtonY + rangeVal * j)
 
-    for i in range(6): #clicks the down arrow 6 times to get to the next coupon
-        pyautogui.click(downArrowX, downArrowY)
-    if numClips == numClipsMax: #helps to stop the precession of the coupons up the page
-        pyautogui.click(downArrowX,downArrowY)
-        pyautogui.click(downArrowX,downArrowY)
-        numClips = 0
-    else:
-        numClips += 1
+    for i in range(numBetweenDownArrows): #clicks the down arrow 6 times to get to the next coupon
+            pyautogui.click(downArrowX, downArrowY)
+
+        # if numClips == numClipsMax: #helps to stop the precession of the coupons up the page
+        #     pyautogui.click(downArrowX,downArrowY)
+        #     pyautogui.click(downArrowX,downArrowY)
+        #     numClips = 0
+        # else:
+        #     numClips += 1
+    
--- clickerTestLocations.py
+++ clickerTestLocations.py
@@ -3,14 +3,24 @@ import time
 
 
 clickButtonX = 850
-clickButtonY = 1000
+clickButtonY = 350
 downArrowX = 955
 downArrowY = 1030
 
+refreshButtonX = 90
+refreshButtonY = 57
 
-for item in range(3):
+numStartingDownArrows = 17 #the number of down arrows you need to click to get to the first coupon
+numBetweenDownArrows = 20 #the number of down arrows you need to click to get to the next coupon
+numClipsMax = 5
+
+rangeMax = 69 #the number of clicks that are in the range
+rangeVal = 10 #the distance between each click
+
+for item in range(numStartingDownArrows): #this it down to get to the first coupon
     pyautogui.click(downArrowX, downArrowY)
 
-
-pyautogui.click(clickButtonX, clickButtonY)
-time.sleep(0.25)
+pyautogui.click(refreshButtonX, refreshButtonY) #refreshes the page
+time.sleep(1)
+for j in range(rangeMax):
+            pyautogui.click(clickButtonX, clickButtonY + rangeVal * j)
